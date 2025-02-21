15ca34265506696b8e241f22de778ce0f8372e34
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-21 19:26:47 +0400

    add NearlyLuckyNumber.java

--- EASY_BOOTCAMP/DAY2/NearlyLuckyNumber.java
+++ EASY_BOOTCAMP/DAY2/NearlyLuckyNumber.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class NearlyLuckyNumber {
     public static void main(String[] args) {
-        solve();
+        // solve1();
+        solve2();
     }
 
-    public static void solve() {
+    public static void solve1() {
         Scanner sc = new Scanner(System.in);
 
         long n = sc.nextLong();
@@ -20,6 +21,28 @@ public class NearlyLuckyNumber {
             if (c == '4' || c == '7') {
                 lucky++;
             }
+        }
+
+        if (lucky == 4 || lucky == 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
+    }
+
+    public static void solve2() {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        int lucky = 0;
+        while (n > 0) {
+            if (n % 10 == 4 || n % 10 == 7) {
+                lucky++;
+            }
+            n /= 10;
         }
 
         if (lucky == 4 || lucky == 7) {
