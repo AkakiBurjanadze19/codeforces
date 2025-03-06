ea3e1201b78ca55fc9839c83299bbb8ffb980507
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-03-07 00:05:32 +0400

    add MulDiv.java

---
+++ LEARN/TRICKS/MulDiv.java
@@ -0,0 +1,21 @@
+package LEARN.TRICKS;
+
+import java.util.Scanner;
+
+public class MulDiv {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        // shift all bits to the left to multiply number by 2
+        int res1 = n << 1;
+        System.out.println(res1);
+
+        // shift all bits to the right to divide number by 2
+        int res2 = n >> 1;
+        System.out.println(res2);
+
+        sc.close();
+    }
+}
