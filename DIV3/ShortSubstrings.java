223433d6b9dbbe731618d7fef5e5fb2b82add666
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-03-06 23:52:03 +0400

    add ShortSubstrings.java

---
+++ DIV3/ShortSubstrings.java
@@ -0,0 +1,40 @@
+package DIV3;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ShortSubstrings {
+    public static void main(String[] args) {
+        solve();
+    }
+
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String b = sc.next();
+
+            ArrayList<String> strs = new ArrayList<>();
+            for (int i = 0; i < b.length() - 1; i++) {
+                String str = "";
+                str += b.charAt(i);
+                str += b.charAt(i + 1);
+
+                i++;
+                strs.add(str);
+            }
+
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < strs.size() - 1; i++) {
+                output.append(strs.get(i).charAt(0));
+            }
+
+            output.append(strs.get(strs.size() - 1));
+            
+            System.out.println(output.toString());
+        }
+
+        sc.close();
+    }
+}
